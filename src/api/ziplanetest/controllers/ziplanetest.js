f535af80c329d22d72769ea226f86909f29eef94
deepaknayak <deepaknayak@ziplanerx.com>
2024-03-21 20:32:49 -0400

    Initial commit

---
+++ src/api/ziplanetest/controllers/ziplanetest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ziplanetest controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ziplanetest.ziplanetest');
---
+++ src/api/ziplanetest/services/ziplanetest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ziplanetest service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ziplanetest.ziplanetest');
